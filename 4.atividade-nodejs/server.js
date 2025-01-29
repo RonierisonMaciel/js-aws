4ef3d45d3bc655218bd00044520362345d32b413
ronierisonmaciel <ronierison.maciel@gmail.com>
2025-01-29 18:53:51 -0300

    :fire: Add main, about, and contact routes to the Express server

--- 4.atividade-nodejs/server.js
+++ 4.atividade-nodejs/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 
+// Rota principal
 app.get('/', (req, res) => {
     res.send('<h1>Olá, mundo!</h1>');
+});
+
+// Rota de sobre
+app.get('/sobre', (req, res) => {
+    res.send('<h1>Sobre</h1><p>Este é um servidor de teste.</p><i>esse é um texto em itálico</i>');
+});
+
+// Rota de contato
+app.get('/contato', (req, res) => {
+    res.send('<h1>Contato</h1><p>Entre em contato conosco pelo <a href="mailto:roni@gmail.com">email</a>');
 });
 
 const PORT = 3000;
